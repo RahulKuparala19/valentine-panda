c4c410a6c181e16e45db01f57c09b5bb562fca05
RahulKuparala19 <89075730+RahulKuparala19@users.noreply.github.com>
2024-02-14 12:34:58 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -11,20 +11,20 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
+    $mail->Host       = 'rahulkuparala@gmail.com';                       //gmail SMTP server set to send through
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'user@example.com';                     //SMTP username (your gmail account)
-    $mail->Password   = 'secret';                               //SMTP password (your gmail password or app password)
+    $mail->Username   = 'rahulkuparala';                     //SMTP username (your gmail account)
+    $mail->Password   = 'Sarada@19042000';                               //SMTP password (your gmail password or app password)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('rahulkuparala@gmail.com', 'Mailer');         //Set the sender of the message (your email address)
+    $mail->addAddress('kuparalarahul19042@gmail.com', 'Joe User');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
-    $mail->Subject = 'I Love You';
+    $mail->Subject = 'Chipi chipi chapa chapa dubi dubi daba daba';
     $mail->Body    = 'Happy Valentine Day My Love. I love you so much. You are the best thing that has ever happened to me ❤';
 
     $mail->send();
